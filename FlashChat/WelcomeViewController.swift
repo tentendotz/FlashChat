95c001edf54c1f244be5ad0a0806ed98df21a748
tentendotz <tenten102matsuyama@gmail.com>
2023-02-06 21:04:25 +0900

    Add typing animation in welcome view.

--- FlashChat/WelcomeViewController.swift
+++ FlashChat/WelcomeViewController.swift
@@ -13,9 +13,33 @@ class WelcomeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        //Typing Animation using for loop
+        titleLabel.text = ""
+        var charIndex = 0.0
+        let titleText = "⚡️FlashChat"
+        for letter in titleText {
+            // for debug print
+            print("-")
+            print(0.1 * charIndex)
+            print(letter)
+            
+            Timer.scheduledTimer(withTimeInterval: 0.1 * charIndex, repeats: false) { (timer) in
+                self.titleLabel.text?.append(letter)
+            }
+            charIndex += 1
+        }
     }
-
+        
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.isNavigationBarHidden = true
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        navigationController?.isNavigationBarHidden = false
+    }
 
 }
 
