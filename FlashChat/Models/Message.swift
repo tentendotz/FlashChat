ab1a8df6a0eb871ea31df91ab2ebf0ad069ba686
tentendotz <tenten102matsuyama@gmail.com>
2023-02-06 21:23:31 +0900

    Add model data

---
+++ FlashChat/Models/Message.swift
@@ -0,0 +1,13 @@
+//
+//  Message.swift
+//  FlashChat
+//
+//  Created by tetsuta matsuyama on 2023/02/06.
+//
+
+import Foundation
+
+struct Message {
+    let sender: String
+    let body: String
+}
