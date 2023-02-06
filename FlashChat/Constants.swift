de81c77736b6597fa72449aca0316d88afaf8821
tentendotz <tenten102matsuyama@gmail.com>
2023-02-06 20:54:49 +0900

    Add constant file

---
+++ FlashChat/Constants.swift
@@ -0,0 +1,28 @@
+//
+//  Constants.swift
+//  FlashChat
+//
+//  Created by tetsuta matsuyama on 2023/02/06.
+//
+
+enum K {
+    static let appName = "⚡️FlashChat"
+    static let cellIdentifier = "ReusableCell"
+    static let cellNibName = "MessageCell"
+    static let registerSegue = "RegisterToChat"
+    static let loginSegue = "LoginToChat"
+    
+    enum BrandColors {
+        static let purple = "BrandPurple"
+        static let lightPurple = "BrandLightPurple"
+        static let blue = "BrandBlue"
+        static let lighBlue = "BrandLightBlue"
+    }
+    
+    enum FStore {
+        static let collectionName = "messages"
+        static let senderField = "sender"
+        static let bodyField = "body"
+        static let dateField = "date"
+    }
+}
