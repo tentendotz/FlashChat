b5d22b61e85c8e9aad4a4cea39518440b9f563f5
tentendotz <tenten102matsuyama@gmail.com>
2023-02-08 20:50:32 +0900

    Add custom component cell

---
+++ FlashChat/Views/MessageCell.swift
@@ -0,0 +1,23 @@
+//
+//  MessageCell.swift
+//  FlashChat
+//
+//  Created by tetsuta matsuyama on 2023/02/06.
+//
+
+import UIKit
+
+class MessageCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
