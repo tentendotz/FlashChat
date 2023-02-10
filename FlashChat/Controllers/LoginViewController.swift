05680603a50443c7ee1e65d6fa930d617241520e
tentendotz <tenten102matsuyama@gmail.com>
2023-02-10 23:23:26 +0900

    Add feature to display the alert for failed log-in

--- FlashChat/Controllers/LoginViewController.swift
+++ FlashChat/Controllers/LoginViewController.swift
@@ -14,9 +14,20 @@ class LoginViewController: UIViewController {
     
     
     //MARK: - Firebase Auth: Log in
+    
     @IBAction func loginPressed(_ sender: UIButton) {
         if emailTextfield.text == "1@2.com" , passwordTextfield.text == "123456" {
             self.performSegue(withIdentifier: K.loginSegue, sender: self)
+        } else {
+            let okAction = UIAlertAction(title: "OK", style: .default)
+            displayAlert(title: "Error", message: "Sorry, couldn't log you in.", actions: [okAction])
         }
     }
+    
+    func displayAlert(title: String, message: String, actions: [UIAlertAction]) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        actions.forEach { alert.addAction($0) }
+        present(alert, animated: true)
+    }
+    
 }
